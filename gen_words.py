bb7a3bd99564dde7a38bdc16b4d9dc6713937b75
Steve Young <fineline@gmail.com>
2021-11-05 17:39:40 -0700

    update gen_words script to return random word range as list

--- gen_words.py
+++ gen_words.py
@@ -7,6 +7,8 @@ from itertools import product
 from string import ascii_lowercase
 import random
 
+#%%
+# For generating letter combos to file
 random.seed(1)
 
 WORD_LEN = 4
@@ -17,7 +19,6 @@ words = ["".join(x) for x in list(product(list(ascii_lowercase), repeat=WORD_LEN
 random.shuffle(words)
 
 
-#%%
 def output_words(low, high):
   """ write out letter combinations in range (low, high) to file"""
   assert 0 <= low < high <= len(words)
@@ -26,6 +27,21 @@ def output_words(low, high):
   with open(os.path.join(OUTPUT_PATH, filename), "w") as f:
     for word in words_to_output:
       f.write(word + "\n")
+
+
+#%%
+# For generating letters combos and returning list
+def return_words(low, high):
+  """ return letter combinations in range (low, high)"""
+  word_len = 4
+  random.seed(1)
+  # all 4 letter combinations
+  all_words = [
+    "".join(x) for x in list(product(list(ascii_lowercase), repeat=word_len))
+  ]
+  random.shuffle(all_words)
+  assert 0 <= low < high <= len(all_words)
+  return all_words[low:high]
 
 
 #%%
